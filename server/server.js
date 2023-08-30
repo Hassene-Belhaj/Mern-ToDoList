e79a835436e38d185f76e94c19d1b9327d10a6d0
Hassene-Belhaj <hassene.belhaj@gmail.com>
2023-08-30 02:46:37 +0200

    add backend & test

--- server/server.js
+++ server/server.js
@@ -1,10 +1,35 @@
 const express = require('express');
 
-const cors = require('cors')
+const cors = require('cors');
 
-const app = express()
+require('./config/connect');
+const Todo = require('./model/Todo') ;
 
-app.use(express.json())
-app.use(cors())
+const app = express();
 
-app.listen('5174' , () => console.log("server is ok"))
+app.use(express.json());
+app.use(cors());
+
+app.listen("5174" , ()=>console.log("server is ok") );
+
+
+app.get( '/' , (req , res) => {
+    res.send('server is running');
+})
+
+
+
+app.get( '/todos' , async (req ,res)=> {
+    const todos = await Todo.find();
+    res.json(todos);
+})
+
+
+app.post( '/new' , async (req , res)=> {
+    const {text} = req.body
+    const newtodo = await new Todo({
+        text : text
+    })
+  await newtodo.save()
+  return res.json({newtodo})
+})
